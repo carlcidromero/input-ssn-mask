6e2ccefb6ba0078d5c72c02809e4e6028fcaeea2
cid <carlcidromero@gmail.com>
2022-10-18 21:25:51 -0700

    Feat: Create POC using phone number mask

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App, { format } from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("MaskedInput", () => {
+  describe("format", () => {
+    const cases = [
+      { value: "", mask: "(###) ###-####", expected: "" },
+      { value: "1", mask: "(###) ###-####", expected: "(1" },
+      { value: "12", mask: "(###) ###-####", expected: "(12" },
+      { value: "123", mask: "(###) ###-####", expected: "(123" },
+      { value: "1234", mask: "(###) ###-####", expected: "(123) 4" },
+      { value: "12345", mask: "(###) ###-####", expected: "(123) 45" },
+      { value: "123456", mask: "(###) ###-####", expected: "(123) 456" },
+      { value: "1234567", mask: "(###) ###-####", expected: "(123) 456-7" },
+      { value: "12345678", mask: "(###) ###-####", expected: "(123) 456-78" },
+      { value: "123456789", mask: "(###) ###-####", expected: "(123) 456-789" },
+      {
+        value: "1234567890",
+        mask: "(###) ###-####",
+        expected: "(123) 456-7890",
+      },
+      {
+        value: "1234567890000",
+        mask: "(###) ###-####",
+        expected: "(123) 456-7890",
+      },
+    ];
+    cases.forEach(({ value, mask, expected }, index) => {
+      it(`test ${index + 1}: ${value} => ${mask}`, () => {
+        expect(format(value, mask)).toBe(expected);
+      });
+    });
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [value, setValue] = useState("");
+  const handleChange = (value: string) => {
+    setValue(value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MaskedInput
+      onChange={handleChange}
+      mask="(###) ###-####"
+      value={value}
+      format={format}
+    />
   );
 }
+
+const MaskedInput: React.FC<MaskedInputProps> = ({
+  mask,
+  value,
+  onChange,
+  format,
+  ...otherProps
+}) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.currentTarget;
+    const cleanValue = value.replace(/[^\d]/g, "");
+    onChange(cleanValue);
+  };
+
+  const formatValue = (value: string, mask: string) => format(value, mask);
+
+  return (
+    <input
+      value={formatValue(value, mask)}
+      onChange={handleChange}
+      {...otherProps}
+    />
+  );
+};
+
+export const format = (value: string, mask: string): string => {
+  let i = 0;
+  let lastReplacedIndex = -1;
+  const filledMask = mask.replace(/#/g, (_, index) => {
+    if (i >= value.length) return "#";
+    lastReplacedIndex = index;
+    return value[i++];
+  });
+  return filledMask.substring(0, lastReplacedIndex + 1);
+};
+
+type MaskedInputProps = Omit<React.HTMLProps<HTMLInputElement>, "onChange"> & {
+  mask: string;
+  value: string;
+  onChange: (value: string) => void;
+  format: (value: string, mask: string) => string;
+};
+
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
 export default App;
